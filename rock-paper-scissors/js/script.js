d8da6374bb3d3a78efe4d05a39ddb3b7b87aa117
Aaron Carlisle <aaroncarlisle.cs@gmail.com>
2024-07-31 10:30:20 -0600

    Create function to get player choice

--- rock-paper-scissors/js/script.js
+++ rock-paper-scissors/js/script.js
@@ -2,10 +2,28 @@ function getComputerChoice() {
     let choice = Math.floor(Math.random() * 3);
     switch (choice) {
         case 0:
-            return 'rock';
+            choice = 'rock';
         case 1:
-            return 'paper';
+            choice = 'paper';
         case 2:
-            return 'scissors';
+            choice = 'scissors';
+    }
+    console.log(`Computer's choice is: ${choice}`);
+    return choice;
+}
+
+function getHumanChoice() {
+    let choice = prompt('Please enter rock, paper, or scissors');
+    choice = choice.toLowerCase();
+    if (choice == 'rock' || choice == 'paper' || choice == 'scissors') {
+        console.log(`Player's choice is: ${choice}`);
+        return choice;
+    }
+    else {
+        alert('Invalid choice!');
+        getHumanChoice();
     }
 }
+
+getComputerChoice();
+getHumanChoice();
